79c602d80aecf637ceb25146ab4f895a9d9adcd5
tsukiraa <tsukira.exists@gmail.com>
2024-07-28 19:14:06 +0200

    1.1fnc blocks on second try

--- blocked_sites.js
+++ blocked_sites.js
@@ -1,11 +1,21 @@
-
 setTimeout(function() {
     var a = document.getElementsByTagName("a")
 console.log(a.length)
 for (let i = 0; i < a.length; i++) {
     const element = a[i];
     console.log(element.href)
-    element.href ="https://afhfuoehf.vondov" ;
+    element.href ="https://www.google.com/" ;
 }
-  }, delayInMilliseconds);    
+
+  }, 10000); 
+  let url = document.URL
+  setInterval(displayHello, 5000);
+
+  function displayHello() {
+    console.log(url)
+    console.log(document.URL)
+    if(url != document.URL){
+        window.location.replace("https://www.google.com/");
+    }
+  }
 
