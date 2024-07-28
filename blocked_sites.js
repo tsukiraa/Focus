d53e9bd3f7b5264eedc0bcfa884b9a44f7f7b3fa
tsukiraa <tsukira.exists@gmail.com>
2024-07-28 13:00:44 +0200

    1.0.1err href changed but still redirects

--- blocked_sites.js
+++ blocked_sites.js
@@ -1,1 +1,11 @@
-window.location.replace('https://www.google.com/');
+
+setTimeout(function() {
+    var a = document.getElementsByTagName("a")
+console.log(a.length)
+for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    console.log(element.href)
+    element.href ="https://afhfuoehf.vondov" ;
+}
+  }, delayInMilliseconds);    
+
---
+++ torch.js
@@ -0,0 +1,5 @@
+console.log("yoou kinda smell like a baka eren jeger")
+window.a = 1;
+while(window.a==1){
+    let delayres = await delay(1000);
+}
