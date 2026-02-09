08f3fd619eb7a190d8e111dac1d7e245a54aafa4
tsukiraa <tsukira.exists@gmail.com>
2026-02-09 21:46:43 +0100

    1.3.1f

--- blocked_stream.js
+++ blocked_stream.js
@@ -3,8 +3,14 @@
 
   function deleteAnything() {
     var a = document.getElementsByClassName("page-container--recommended")
+    var b = document.getElementsByClassName("category__links-container")
+    var c = document.getElementsByClassName("ribbon-wrapper")
+    var d = document.getElementsByClassName("origin-tag")
     console.log(a.length)
     for (let i = 0; i < a.length; i++) {
         a[i].remove()
+        b[i].remove()
+        c[i].remove()//god i hate this
+        d[i].remove()
     }
   }
